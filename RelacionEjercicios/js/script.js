d010e75f70412ac9e28583cc9f9f583986212d41
James Abreu <jaas210299@gmail.com>
2024-10-09 13:44:55 +0200

    Ejercicios hechos hasta el 6

--- RelacionEjercicios/js/script.js
+++ RelacionEjercicios/js/script.js
@@ -16,37 +16,48 @@ function ej1() {
   textEj1.textContent = array1;
 
   console.log(array1); // [0, 7, 1, 1, 3, 4, 1, 1, 6, 1, 1, 8, 1, 1]
+
+  document.getElementById("btn-ej1").addEventListener("click", ej1);
+
 }
-document.getElementById("btn-ej1").addEventListener("click", ej1);
+
 
 // Ejercicio 2
 // Crear un vector de 10 elementos con valores “aleatorios” comprendidos entre 0 y 1000. Buscar el menor y luego generar otro vector a partir de dicha posición hasta el final.  Para manipular el vector debes de hacer uso de la función “slice”. También debes de buscar por internet como generar números random en javascript comprendidos en un rango determinado.
 function ej2() {
   let array2 = [];
   let array3 = [];
-  let arrayAux = [];
-  let min = 0;
+
+ 
+  let ind = 0;
   for (let index = 0; index < 10; index++) {
+    //Asignamos 10 valores aleatorios de 1 al 100 a nuestro array
     array2.push(Math.floor(Math.random() * 1001));
-    min = Math.min(min, index);
+    //Tenemos que sacar el menor valor del array
+    
+    
+  }
+  let min = array2[0];
+  for (let index = 0; index < array2.length; index++) {
+    if(array2[index] < min){
+      min = array2[index];
+      ind++;  
+    }
   }
 
-  for (let index = 0; index < array2.length; index++) {
-    array3.push(array2[min]);
-    min++;
-  }
-  
+  array3 = array2.slice(ind, array2.length);
+
   console.log(array2);
 
   let textEj2 = document.getElementById("h1-ej2");
 
   textEj2.textContent =
-    "Números aleatorios: del 0 al 1000: " +
-    array2 +
-    " Números mayores al menor: " +
-    array3;
+    `Números aleatorios: del 0 al 1000: ${array2} Números mayores al menor: ${array3}`;
+  
+    document.getElementById("btn-ej2").addEventListener("click", ej2);
+  
 }
-document.getElementById("btn-ej2").addEventListener("click", ej2);
+
 
 // Ejercicio 3
 // Escribe una función capaz de fusionar 2 arrays y que elimine todos los elementos duplicados.
@@ -62,8 +73,17 @@ console.log(array);
 
 // Salida:      [3, 2, 30, 1]
 
+
+
+
 // Ejercicio 4 - DNI
 // El cálculo de la letra del Documento Nacional de Identidad (DNI) es un proceso matemático sencillo que se basa en obtener el resto de la división entera del número de DNI y el número 23. A partir del resto de la división, se obtiene la letra seleccionandola dentro de un array de letras.
+// Por tanto si el resto de la división es 0, la letra del DNI es la T y si el resto es 3 la letra es la A.
+// Con estos datos, elaborar un pequeño script que:
+// 1. Almacene en una variable el número de DNI indicado por el usuario y en otra variable la letra del DNI que se ha indicado. (Pista: si se quiere pedir directamente al usuario que indique su número y su letra, se puede utilizar la función prompt())
+// 2. En primer lugar (y en una sola instrucción) se debe comprobar si el número es mayor que 0 o menor que 99999999. Si ese es el caso, se muestra un mensaje al usuario indicando que el número proporcionado no es válido y el programa no muestra más mensajes.
+// 3. Si el número es válido, se calcula la letra que le corresponde según el método explicado anteriormente.
+// 4. Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si no coinciden, se muestra un mensaje al usuario diciéndole que la letra que ha indicado no es correcta. En otro caso, se muestra un mensaje indicando que el número y la letra de DNI son correctos.
 // El array de letras es:
 function ej3() {
   let letras = [
@@ -94,31 +114,14 @@ function ej3() {
   ];
 
   let nDNI = prompt("Ingresa el número de tu DNI");
-   let text = document.getElementById("h1-ej3");
+  let text = document.getElementById("h1-ej3");
 
-   nDNI = parseInt(nDNI);
-   text.textContent = "Su letra de DNI es: " + letras[nDNI % 23];
+  nDNI = parseInt(nDNI);
+  text.textContent = "Su letra de DNI es: " + letras[nDNI % 23];
+  document.getElementById("btn-ej3").addEventListener("click", ej3);
 }
-document.getElementById("btn-ej3").addEventListener("click", ej3);
 
-// Por tanto si el resto de la división es 0, la letra del DNI es la T y si el resto es 3 la letra es la A.
-// Con estos datos, elaborar un pequeño script que:
-// 1. Almacene en una variable el número de DNI indicado por el usuario y en otra variable la letra del DNI que se ha indicado. (Pista: si se quiere pedir directamente al usuario que indique su número y su letra, se puede utilizar la función prompt())
-// 2. En primer lugar (y en una sola instrucción) se debe comprobar si el número es mayor que 0 o menor que 99999999. Si ese es el caso, se muestra un mensaje al usuario indicando que el número proporcionado no es válido y el programa no muestra más mensajes.
-// 3. Si el número es válido, se calcula la letra que le corresponde según el método explicado anteriormente.
-// 4. Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si no coinciden, se muestra un mensaje al usuario diciéndole que la letra que ha indicado no es correcta. En otro caso, se muestra un mensaje indicando que el número y la letra de DNI son correctos.
 
-// Ejercicio 4 - DNI
-// El cálculo de la letra del Documento Nacional de Identidad (DNI) es un proceso matemático sencillo que se basa en obtener el resto de la división entera del número de DNI y el número 23. A partir del resto de la división, se obtiene la letra seleccionandola dentro de un array de letras.
-// El array de letras es:
-// let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
-
-// Por tanto si el resto de la división es 0, la letra del DNI es la T y si el resto es 3 la letra es la A.
-// Con estos datos, elaborar un pequeño script que:
-// 1. Almacene en una variable el número de DNI indicado por el usuario y en otra variable la letra del DNI que se ha indicado. (Pista: si se quiere pedir directamente al usuario que indique su número y su letra, se puede utilizar la función prompt())
-// 2. En primer lugar (y en una sola instrucción) se debe comprobar si el número es mayor que 0 o menor que 99999999. Si ese es el caso, se muestra un mensaje al usuario indicando que el número proporcionado no es válido y el programa no muestra más mensajes.
-// 3. Si el número es válido, se calcula la letra que le corresponde según el método explicado anteriormente.
-// 4. Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si no coinciden, se muestra un mensaje al usuario diciéndole que la letra que ha indicado no es correcta. En otro caso, se muestra un mensaje indicando que el número y la letra de DNI son correctos.
 
 // Ejercicio 5
 // Realiza las siguientes operaciones con un vector/array:
@@ -129,6 +132,18 @@ document.getElementById("btn-ej3").addEventListener("click", ej3);
 // Añade por el principio “Pera”.
 // Muestra todo el vector.
 // Muestra todo el vector ordenado alfabéticamente.
+function ej5(){
+  let text = document.getElementById("h1-ej5");
+  let array2 = ['Sandía', 'Melón'];
+  array2.push('Kiwi');
+  let valorMedio =  Math.floor(array2.length / 2);
+  text.textContent = `Mi array es: ${array2},`;
+  text.textContent += ` el valor del medio del array es: ${array2[valorMedio]}`
+  array2[valorMedio] = 'Melocotón';
+  text.textContent += ` añadiendo melocotón en el medio queda así: ${array2}`;
+
+}
+document.getElementById("btn-ej5").addEventListener("click", ej5);
 
 // Ejercicio 6 - Código de rotación
 // El objetivo de esta actividad es utilizar alguna de las funciones predefinidas del lenguaje JavaScript. Puede utilizar las funciones: chat, indexOf, alert, length, prompt y toUpperCase.
@@ -137,10 +152,39 @@ document.getElementById("btn-ej3").addEventListener("click", ej3);
 // Alfabeto original: ABCDEFGHIKLMNOPQRSTV
 // Alfabeto cifrado: DEFGHIKLMNOPQRSTVABC
 // Este tipo de cifrado es un código de rotación, donde la clave de rotación es 3. Escribe un programa que permita cifrar utilizando un código de rotación.
+function ej6(){
+  let origStr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V"];
+  let cifStr = ["D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "A", "B", "C"];
+  let sal = document.getElementById("h1-ej6");
+  let texto = prompt("Dime tu texto:")
+  let textoCif ='';
 
+  for (let index = 0; index < texto.length; index++) {
+    let char = texto.charAt(index).toUpperCase();
+    let idx = origStr.indexOf(char);
+    if(idx !== -1){
+      textoCif += cifStr[idx];
+    }
+    else{
+      textoCif += char;
+    }
+  }
+  sal.textContent = `Texto cifrado: ${textoCif}`;
+}
+document.getElementById("btn-ej6").addEventListener("click", ej6);
 // Ejercicio 7 - Juego de los animales
 // En esta actividad debe codificar una web HTML junto a un script donde realizaremos un pequeño juego en el que se pregunta al usuario a qué especie pertenece cada animal.
 // 1º. Se define en un array una lista de animales, y en otro array la lista de la clase a la que pertenecen los animales del primer array: mamíferos, pájaros, anfibios, reptiles, peces. Ambos arrays tienen el mismo número de elementos.
 // 2º. Seguidamente definirá un bucle, y en cada pase del bucle se pregunta al usuario a qué clase pertenece el animal, escogido de forma aleatoria.
 // 3º. Debe haber un contador de preguntas contestadas, otro de preguntas correctas y otro de preguntas incorrectas.
 // 4º. También debe implementar alguna condición/mecanismo de salida del bucle.
+function ej7(){
+  let text = document.getElementById('h1-ej7');
+
+
+
+  text.textContent = "  assad";
+  console.log(text);
+  
+}
+document.getElementById("btn-ej7").addEventListener("click", ej7);
