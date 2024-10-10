4b31cb9709a71673cc6e3a971d172fae17ddd4b6
James Abreu <jaas210299@gmail.com>
2024-10-10 14:25:42 +0200

    ejercicio 2 hecho de typescript

---
+++ ts/typescript/tarea/ej1.js
@@ -0,0 +1,4 @@
+var nombre = "Nestor Kauil";
+var edad = 25;
+var personaje = ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"];
+console.log("Nombre: ".concat(nombre, " edad: ").concat(edad, " personaje: ").concat(personaje));
---
+++ ts/typescript/tarea/ej1.ts
@@ -0,0 +1,6 @@
+
+let nombre:string = "Nestor Kauil";
+let edad:number = 25;
+let personaje:string[] = ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"];
+
+console.log(`Nombre: ${nombre} edad: ${edad} personaje: ${personaje}`);
