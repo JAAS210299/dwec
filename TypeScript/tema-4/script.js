4d1bb1c246b16d5b4b2ce27c554c546e537fc775
James Abreu <jaas210299@gmail.com>
2024-11-07 14:25:09 +0100

    hecho hasta el 4.1 y hay que revisarlo

--- TypeScript/tema-4/script.js
+++ TypeScript/tema-4/script.js
@@ -9,69 +9,68 @@ const multiplicarNumeros = () => {
             title: "Mi menu"
         }];
     const propiedades = menu.map(item => {
-        if (typeof item.width === 'number') {
-            item.width * 2;
+        if (typeof item.width === 'number' && typeof item.height === 'number') {
+            item.width *= 2;
+            item.height *= 2;
         }
         return item;
     });
     const menuU = JSON.stringify(propiedades);
     alert(menuU);
 };
-// // // Después de la llamada
-// // // menu = { width: 400,  height: 600,  title: "Mi menu" };
-// // La función no debe de devolver ningún tipo de valor.
-// // Debes de mirar si los valores son numéricos o no antes de multiplicarle su valor por 2.
-// let menu: Window | null = null;
-// const multiplicarNumeros = (menu: Window | null): Window | null => {
-//     let altura: number = 44;
-//     let anchura: number = 44;
-//     if (menu === null || menu.closed) {
-//         menu = window.open(
-//             '', // URL vacía para abrir una nueva ventana
-//             'nombreVentana', // Nombre de la ventana
-//             `height=${altura},width=${anchura}` // Características de la ventana
-//         );
-//     }
-//     else{
-//         altura *= 2; // Duplica la altura
-//         anchura *= 2; // Duplica la anchura
-//         menu.resizeTo(anchura, altura);
-//     }
-//     return menu;
-// };
 // // Ejercicio 2
 // // Tenemos un objeto que guarda los salarios de un equipo de trabajo:
-// // let salarios = [{
-// //   	Juan: 10000,
-// //   	Alicia: 16000,
-// //   	Lucas: 13000
-// // }]
 // // Escribe un script para sumar estos salarios y mostrar el resultado de la suma.
+const totalSalario = () => {
+    let salarios = [{
+            Juan: 10000,
+            Alicia: 16000,
+            Lucas: 13000
+        }];
+    const totalSalarios = salarios.reduce((acumulador, empleados) => acumulador + empleados.Alicia + empleados.Juan + empleados.Lucas, 0);
+    alert(totalSalarios);
+};
 // // Ejercicio 3
 // // Dado el siguiente array de objetos: 
-// // const iceCreams = [
-// //   { flavor: 'pineapple', color: 'white' },
-// //   { flavor: 'strawberry', color: 'red' },
-// //   { flavor: 'watermelon', color: 'red' },
-// //   { flavor: 'kiwi', color: 'green' },
-// //   { flavor: 'mango', color: 'yellow' },
-// //   { flavor: 'pear', color: 'green' }
-// // ];
+const iceCreams = [
+    { flavor: 'pineapple', color: 'white' },
+    { flavor: 'strawberry', color: 'red' },
+    { flavor: 'watermelon', color: 'red' },
+    { flavor: 'kiwi', color: 'green' },
+    { flavor: 'mango', color: 'yellow' },
+    { flavor: 'pear', color: 'green' }
+];
 // // 3.1 .  ¿Cómo podría obtener un array con solo los  helados que son de color rojo ?
+const saboresHelados_rojos = () => {
+    let sabores = iceCreams.filter(iceCream => iceCream.color === 'red').map(iceCream => iceCream.flavor);
+    sabores = JSON.stringify(sabores);
+    alert('¿Cómo podría obtener un array con solo los  helados que son de color rojo ? ' + sabores);
+};
 // // 3.2.  ¿ Cómo podríamos obtener un array que solo contenga los sabores de los helados tal como [ “pineaple” , “watermelon”, “kiwi”, ……..etc ]  ?
+const saboresHelados = () => {
+    alert(iceCreams.map(item => item.flavor));
+};
 // // Ejercicio 4
 // // Dado la siguiente estructura de datos sobre pilotos y años de experiencia:
-// // let pilotos = [
-// //   {
-// // 	id: 10,
-// // 	name: "Poe Dameron",
-// // 	years: 14,
-// //   },
-// //   {
-// // 	id: 2,
-// // 	name: "Temmin 'Snap' Wexley",
-// // 	years: 30,
-// //   },
+let pilotos = [
+    {
+        id: 10,
+        name: "Poe Dameron",
+        years: 14,
+    },
+    {
+        id: 2,
+        name: "Temmin 'Snap' Wexley",
+        years: 30,
+    },
+];
+// 4.1. ¿Cómo podríamos obtener los años totales de experiencia de todos los pilotos juntos?
+const totalYears = () => {
+    let totality = pilotos.map(item => item.years);
+    alert(totality);
+};
+// 4.2. Realice la misma operación del apartado anterior (4.1) pero haciendo uso del operador “arrow”:
+// 4.3 Ahora vamos a tratar de obtener el piloto con más experiencia. (Esta hay que darle un par de vueltas).
 // Ejercicio 5
 // Dado la siguiente estructura de datos sobre pilotos con id, nombre y su división (faction):
 // var pilots = [
