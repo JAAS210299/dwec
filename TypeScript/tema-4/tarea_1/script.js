7d4bd47e92adc8df426e907418f8927f55fb280c
James Abreu <jaas210299@gmail.com>
2024-11-11 12:42:41 +0100

    Tarea 1 del tema 4 realizada completa

--- TypeScript/tema-4/tarea_1/script.js
+++ TypeScript/tema-4/tarea_1/script.js
@@ -65,90 +65,100 @@ let pilotos = [
     },
 ];
 // 4.1. ¿Cómo podríamos obtener los años totales de experiencia de todos los pilotos juntos?
+function aniosTotales() {
+    let totality = pilotos.reduce((acumulador, piloto) => acumulador + piloto.years, 0);
+    alert(totality);
+}
+// 4.2. Realice la misma operación del apartado anterior (4.1) pero haciendo uso del operador “arrow”:
 const totalYears = () => {
-    let totality = pilotos.map(item => item.years);
+    let totality = pilotos.reduce((acumulador, piloto) => acumulador + piloto.years, 0);
     alert(totality);
 };
-// 4.2. Realice la misma operación del apartado anterior (4.1) pero haciendo uso del operador “arrow”:
 // 4.3 Ahora vamos a tratar de obtener el piloto con más experiencia. (Esta hay que darle un par de vueltas).
+const masExperiencia = () => {
+    let mayorTiempo = Math.max(...pilotos.map(piloto => piloto.years));
+    alert(mayorTiempo);
+};
 // Ejercicio 5
 // Dado la siguiente estructura de datos sobre pilotos con id, nombre y su división (faction):
-// var pilots = [
-//   {
-// 	id: 2,
-// 	name: "Wedge Antilles",
-// 	faction: "Rebels",
-//   },
-//   {
-// 	id: 8,
-// 	name: "Ciena Ree",
-// 	faction: "Empire",
-//   },
-// {
-// 	id: 40,
-// 	name: "Iden Versio",
-// 	faction: "Empire",
-//   },
-//   {
-// 	id: 66,
-// 	name: "Thane Kyrell",
-// 	faction: "Rebels",
-//   }
-// ];
+const pilots = [
+    {
+        id: 2,
+        name: "Wedge Antilles",
+        faction: "Rebels",
+    },
+    {
+        id: 8,
+        name: "Ciena Ree",
+        faction: "Empire",
+    },
+    {
+        id: 40,
+        name: "Iden Versio",
+        faction: "Empire",
+    },
+    {
+        id: 66,
+        name: "Thane Kyrell",
+        faction: "Rebels",
+    }
+];
 // 5.1. ¿Cómo podría obtener los pilotos que fueran solo Rebeldes  (Rebels)?
+function pilotosRebeldesFunction() {
+    const rebeldes = pilots.filter(pilotos => pilotos.faction === "Rebels");
+    alert(JSON.stringify(rebeldes));
+}
 // 5.2. Realice la misma operación del apartado 5.1 pero haciendo uso del operador “arrow”.
+const pilotosRebeldesArrow = () => {
+    const rebeldes = pilots.filter(pilotos => pilotos.faction === "Rebels");
+    alert(JSON.stringify(rebeldes));
+};
 // Ejercicio 6
 // Dado la siguiente estructura de datos sobre personajes
-// var personnel = [
-//   {
-// 	id: 5,
-// 	name: "Luke Skywalker",
-// 	pilotingScore: 98,
-// 	shootingScore: 56,
-// 	isForceUser: true,
-//   },
-//   {
-// 	id: 82,
-// 	name: "Sabine Wren",
-// 	pilotingScore: 73,
-// 	shootingScore: 99,
-// 	isForceUser: false,
-//   },
-//   {
-// 	id: 22,
-// 	name: "Zeb Orellios",
-// 	pilotingScore: 20,
-// 	shootingScore: 59,
-// 	isForceUser: false,
-//   },
-// {
-// 	id: 15,
-// 	name: "Ezra Bridger",
-// 	pilotingScore: 43,
-// 	shootingScore: 67,
-// 	isForceUser: true,
-//   },
-//   {
-// 	id: 11,
-// 	name: "Caleb Dume",
-// 	pilotingScore: 71,
-// 	shootingScore: 85,
-// 	isForceUser: true,
-//   },
-// ];
+const personnel = [
+    {
+        id: 5,
+        name: "Luke Skywalker",
+        pilotingScore: 98,
+        shootingScore: 56,
+        isForceUser: true,
+    },
+    {
+        id: 82,
+        name: "Sabine Wren",
+        pilotingScore: 73,
+        shootingScore: 99,
+        isForceUser: false,
+    },
+    {
+        id: 22,
+        name: "Zeb Orellios",
+        pilotingScore: 20,
+        shootingScore: 59,
+        isForceUser: false,
+    },
+    {
+        id: 15,
+        name: "Ezra Bridger",
+        pilotingScore: 43,
+        shootingScore: 67,
+        isForceUser: true,
+    },
+    {
+        id: 11,
+        name: "Caleb Dume",
+        pilotingScore: 71,
+        shootingScore: 85,
+        isForceUser: true,
+    },
+];
 // 6.1. Intente obtener el total de todas las puntuaciones (tanto pilotingScore como shootingScore juntas) de todos aquellos personajes que sean solo usuarios de la fuerza (isForceUser == true).   (CONSEJO: Aquí va a necesitar encadenar diversos operadores).
+function ej6_1() {
+    const totalPuntuaciones = personnel.filter(personal => personal.isForceUser === true).map(salarios => salarios.pilotingScore + salarios.shootingScore).reduce((total, puntuacion) => total + puntuacion, 0);
+    alert(`Total puntuaciones que usan la fuerza: ${totalPuntuaciones}`);
+}
 // 6.2.  Realice lo anterior pero haciendo uso esta vez del operador arrow (=>). 
-// // {
-// // 	id: 41,
-// // 	name: "Tallissan Lintra",
-// // 	years: 16,
-// //   },
-// //   {
-// // 	id: 99,
-// // 	name: "Ello Asty",
-// // 	years: 22,
-// //   }
-// // ];
-// // 4.1. ¿Cómo podríamos obtener los años totales de experiencia de todos los pilotos juntos?
-// // 4.2. Realice la misma operación del apartado anterior (4.1) pero haciendo uso del operador “arrow”:
-// // 4.3 Ahora vamos a tratar de obtener el piloto con más experiencia. (Esta hay que darle un par de vueltas).
+const ej6_2 = () => {
+    const totalPuntuaciones = personnel.filter(personal => personal.isForceUser === true).map(salarios => salarios.pilotingScore + salarios.shootingScore).reduce((total, puntuacion) => total + puntuacion, 0);
+    alert(`Total puntuaciones que usan la fuerza: ${totalPuntuaciones}`);
+};
--- TypeScript/tema-4/tarea_1/script.ts
+++ TypeScript/tema-4/tarea_1/script.ts
@@ -79,112 +79,111 @@ let pilotos: {id:number, name:string, years:number}[] = [
 ];
 
 // 4.1. ¿Cómo podríamos obtener los años totales de experiencia de todos los pilotos juntos?
-const totalYears = ():void =>{
-    let totality: number | number[] = pilotos.reduce((acumulador, piloto) => acumulador + piloto.years , 0)
-    alert(totality);
-
+function aniosTotales():void{
+  let totality: number | number[] = pilotos.reduce((acumulador, piloto) => acumulador + piloto.years , 0)
+  alert(totality);
 }
 // 4.2. Realice la misma operación del apartado anterior (4.1) pero haciendo uso del operador “arrow”:
-// 4.3 Ahora vamos a tratar de obtener el piloto con más experiencia. (Esta hay que darle un par de vueltas).
+const totalYears = ():void =>{
+  let totality: number | number[] = pilotos.reduce((acumulador, piloto) => acumulador + piloto.years , 0)
+  alert(totality);
 
+}
+// 4.3 Ahora vamos a tratar de obtener el piloto con más experiencia. (Esta hay que darle un par de vueltas).
+const masExperiencia = ():void => {
+  let mayorTiempo: number | number [] = Math.max(...pilotos.map(piloto => piloto.years));
+  alert(mayorTiempo);
+}
 // Ejercicio 5
 // Dado la siguiente estructura de datos sobre pilotos con id, nombre y su división (faction):
-// var pilots = [
-//   {
-// 	id: 2,
-// 	name: "Wedge Antilles",
-// 	faction: "Rebels",
-//   },
-//   {
-// 	id: 8,
-// 	name: "Ciena Ree",
-// 	faction: "Empire",
-//   },
-  
-// {
-// 	id: 40,
-// 	name: "Iden Versio",
-// 	faction: "Empire",
-//   },
-//   {
-// 	id: 66,
-// 	name: "Thane Kyrell",
-// 	faction: "Rebels",
-//   }
-// ];
+ const pilots = [
+  {
+	  id: 2,
+	  name: "Wedge Antilles",
+	  faction: "Rebels",
+  },
+  { 
+    id: 8,
+	  name: "Ciena Ree",
+	  faction: "Empire",
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire",
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels",
+  }
+];
 
 
 
 // 5.1. ¿Cómo podría obtener los pilotos que fueran solo Rebeldes  (Rebels)?
+function pilotosRebeldesFunction():void {
+  const rebeldes = pilots.filter(pilotos => pilotos.faction === "Rebels");
+  alert(JSON.stringify(rebeldes));
+}
 // 5.2. Realice la misma operación del apartado 5.1 pero haciendo uso del operador “arrow”.
-
+const pilotosRebeldesArrow = ():void => {
+  const rebeldes = pilots.filter(pilotos => pilotos.faction === "Rebels");
+  alert(JSON.stringify(rebeldes));
+}
 // Ejercicio 6
 // Dado la siguiente estructura de datos sobre personajes
-// var personnel = [
-//   {
-// 	id: 5,
-// 	name: "Luke Skywalker",
-// 	pilotingScore: 98,
-// 	shootingScore: 56,
-// 	isForceUser: true,
-//   },
-//   {
-// 	id: 82,
-// 	name: "Sabine Wren",
-// 	pilotingScore: 73,
-// 	shootingScore: 99,
-// 	isForceUser: false,
-//   },
-//   {
-// 	id: 22,
-// 	name: "Zeb Orellios",
-// 	pilotingScore: 20,
-// 	shootingScore: 59,
-// 	isForceUser: false,
-//   },
+const personnel = [
+  {
+	id: 5,
+	name: "Luke Skywalker",
+	pilotingScore: 98,
+	shootingScore: 56,
+	isForceUser: true,
+  },
+  {
+	id: 82,
+	name: "Sabine Wren",
+	pilotingScore: 73,
+	shootingScore: 99,
+	isForceUser: false,
+  },
+  {
+	id: 22,
+	name: "Zeb Orellios",
+	pilotingScore: 20,
+	shootingScore: 59,
+	isForceUser: false,
+  },
   
 
-// {
-// 	id: 15,
-// 	name: "Ezra Bridger",
-// 	pilotingScore: 43,
-// 	shootingScore: 67,
-// 	isForceUser: true,
-//   },
-//   {
-// 	id: 11,
-// 	name: "Caleb Dume",
-// 	pilotingScore: 71,
-// 	shootingScore: 85,
-// 	isForceUser: true,
-//   },
-// ];
+{
+	id: 15,
+	name: "Ezra Bridger",
+	pilotingScore: 43,
+	shootingScore: 67,
+	isForceUser: true,
+  },
+  {
+	id: 11,
+	name: "Caleb Dume",
+	pilotingScore: 71,
+	shootingScore: 85,
+	isForceUser: true,
+  },
+];
 
 
 
 // 6.1. Intente obtener el total de todas las puntuaciones (tanto pilotingScore como shootingScore juntas) de todos aquellos personajes que sean solo usuarios de la fuerza (isForceUser == true).   (CONSEJO: Aquí va a necesitar encadenar diversos operadores).
+function ej6_1():void {
+  const totalPuntuaciones =  personnel.filter(personal => personal.isForceUser === true).map(salarios  =>  salarios.pilotingScore + salarios.shootingScore) .reduce((total, puntuacion) => total + puntuacion, 0);
+  alert(`Total puntuaciones que usan la fuerza: ${totalPuntuaciones}`);
+}
 // 6.2.  Realice lo anterior pero haciendo uso esta vez del operador arrow (=>). 
+const ej6_2 = ():void => {
+  const totalPuntuaciones =  personnel.filter(personal => personal.isForceUser === true).map(salarios  =>  salarios.pilotingScore + salarios.shootingScore) .reduce((total, puntuacion) => total + puntuacion, 0);
+  alert(`Total puntuaciones que usan la fuerza: ${totalPuntuaciones}`);
+}
 
-
-
-
-
-
-  
-
-// // {
-// // 	id: 41,
-// // 	name: "Tallissan Lintra",
-// // 	years: 16,
-// //   },
-// //   {
-// // 	id: 99,
-// // 	name: "Ello Asty",
-// // 	years: 22,
-// //   }
-// // ];
-
-// // 4.1. ¿Cómo podríamos obtener los años totales de experiencia de todos los pilotos juntos?
-// // 4.2. Realice la misma operación del apartado anterior (4.1) pero haciendo uso del operador “arrow”:
-// // 4.3 Ahora vamos a tratar de obtener el piloto con más experiencia. (Esta hay que darle un par de vueltas).
 
