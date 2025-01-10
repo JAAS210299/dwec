186c05b4738843d46b5d4aee77c23c31e7d88821
James Abreu <jaas210299@gmail.com>
2025-01-10 13:41:47 +0100

    revisar en casa

--- proyecto-calculadora-moneda/client/js/script.js
+++ proyecto-calculadora-moneda/client/js/script.js
@@ -1,6 +1,6 @@
 const llamandoAPI = async () => {
   try {
-    const respuesta = await fetch('http://127.0.0.1:3000/perras');
+    const respuesta = await fetch("http://127.0.0.1:3000/perras");
     const datos = await respuesta.json();
     console.log(datos);
   } catch (error) {
@@ -10,6 +10,16 @@ const llamandoAPI = async () => {
 
 llamandoAPI();
 
-
 // CAMBIO DE MONEDA CON MONTO INCLUIDO
 // https://v6.exchangerate-api.com/v6/3c2fe95af7bec6d9be69ca76/pair/EUR/GBP/AMOUNT
+const cambioDeMoneda = async (monedaOrigen, monedaDestino, monto) => {
+  try {
+    const respuesta = await fetch(`https://v6.exchangerate-api.com/v6/3
+      c2fe95af7bec6d9be69ca76/pair/${monedaOrigen}/${monedaDestino}/${monto}`);
+    const datos = await respuesta.json();
+    console.log(datos);
+  } catch (error) {
+    console.log(error);
+  }
+};
+cambioDeMoneda("EUR", "GBP", 100);
